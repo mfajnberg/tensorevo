82b08b440f2abc687eb9aa06609be57d5e80305c
Daniil Fajnberg <mail@daniil.fajnberg.de>
2024-01-30 13:28:14 +0100

    Write an add-connection-mutation function

--- src/world.rs
+++ src/world.rs
@@ -1,11 +1,13 @@
 use std::cmp::{min, max};
 use std::collections::HashMap;
 
+use num_traits::{One, Zero};
 use ordered_float::OrderedFloat;
-use rand::distributions::Slice;
+use rand::distributions::{Slice, WeightedIndex};
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 use rand::{Rng, thread_rng};
+use rand_distr::{Distribution, Poisson};
 
 use crate::component::TensorComponent;
 use crate::individual::Individual;
@@ -104,8 +106,8 @@ pub fn select<T: Tensor>(world: &mut World<T>, species_key: &str) -> Vec<(usize,
 }
 
 
-fn init_weight<C: TensorComponent>(rng: &mut ThreadRng) -> Option<C> {
-    if *[true, false].choose(rng).unwrap() {
+fn init_weight<C: TensorComponent>(rng: &mut ThreadRng, can_zero: bool) -> Option<C> {
+    if can_zero && *[true, false].choose(rng).unwrap() {
         Some(C::zero())
     } else {
         C::from_f32(rng.gen_range(0f32..1f32))
@@ -138,7 +140,7 @@ fn crossover_layer_weights<T: Tensor>(
         }
         // neither has a component at row|col
         else {
-            *component = init_weight(rng).unwrap()
+            *component = init_weight(rng, true).unwrap()
         }
     }
 }
@@ -193,7 +195,84 @@ pub fn procreate<T: Tensor>(parent_1: &Individual<T>, parent_2: &Individual<T>) 
         layers.push(Layer::new(weights, biases, activation));
         num_cols = num_rows;
     }
+    mutate_add_connections(&mut layers, &mut rng);
     Individual::new(layers, parent_1.get_cost_function().clone())
+}
+
+
+/// Returns a vector that maps "global" neuron index to 2-tuple of layer index and "in-layer" neuron index.
+pub fn mutate_add_connections<T: Tensor>(layers: &mut Vec<Layer<T>>, rng: &mut ThreadRng) {
+    let neuron_index_lookup = get_neuron_index_lookup(layers);
+    let total_size = neuron_index_lookup.len();
+    // Decide how many new connections to create.
+    let dist = Poisson::new(1.).unwrap();
+    let num_new_connections = dist.sample(rng) as usize;
+    (0..num_new_connections).for_each(|_| add_new_connection(layers, total_size, &neuron_index_lookup, rng));
+}
+
+
+fn get_neuron_index_lookup<T: Tensor>(layers: &Vec<Layer<T>>) -> Vec<(usize, usize)> {
+    let size = layers.iter().map(|layer| layer.size()).sum();
+    let mut neuron_index_lookup = Vec::with_capacity(size);
+    for (layer_idx, layer) in layers.iter().enumerate() {
+        for neuron_idx in 0..layer.size() {
+            neuron_index_lookup.push((layer_idx, neuron_idx))
+        }
+    }
+    neuron_index_lookup
+}
+
+
+/// Randomly chooses a start and end of a new connection.
+fn choose_new_connection<T: Tensor>(
+    layers: &Vec<Layer<T>>,
+    total_size: usize,
+    neuron_index_lookup: &Vec<(usize, usize)>,
+    rng: &mut ThreadRng,
+) -> (usize, usize, usize, usize) {
+    let global_idx = rng.gen_range(0..total_size);
+    let (mut start_layer_idx, mut start_neuron_idx) = neuron_index_lookup[global_idx];
+    let mut distribution_weights: Vec<f32> = layers.iter().map(|layer| layer.size() as f32).collect();
+    for index in 0..distribution_weights.len() {
+        let distance = index as isize - start_layer_idx as isize; // cast could theoretically lead to wraparound if there is an absurd amount of layers
+        distribution_weights[index] *= if distance == 0 { 0. } else { 2f32.powi((1-distance.abs()) as i32) };
+    }
+    let dist = WeightedIndex::new(&distribution_weights).unwrap();
+    let mut end_layer_idx = dist.sample(rng);
+    let mut end_neuron_idx = rng.gen_range(0..layers[end_layer_idx].size());
+    if end_layer_idx < start_layer_idx {
+        (end_layer_idx, end_neuron_idx, start_layer_idx, start_neuron_idx) = (start_layer_idx, start_neuron_idx, end_layer_idx, end_neuron_idx);
+    }
+    (start_layer_idx, start_neuron_idx, end_layer_idx, end_neuron_idx)
+}
+
+
+/// Creates new connections and possibly neurons in between.
+fn add_new_connection<T: Tensor>(
+    layers: &mut Vec<Layer<T>>,
+    total_size: usize,
+    neuron_index_lookup: &Vec<(usize, usize)>,
+    rng: &mut ThreadRng,
+) {
+    let (start_layer_idx, start_neuron_idx, end_layer_idx, end_neuron_idx) = choose_new_connection(layers, total_size, neuron_index_lookup, rng);
+    let mut prev_neuron_idx = start_neuron_idx;
+    for layer_idx in (start_layer_idx + 1)..end_layer_idx {
+        // Append a row to weight matrix of intermediate layer.
+        // The row is all zeros except for the column corresponding to the neuron connected to
+        // it from the previous layer.
+        let weights = &mut layers[layer_idx].weights;
+        let mut new_row = vec![T::Component::zero(); weights.shape().1];
+        new_row[prev_neuron_idx] = T::Component::one();
+        weights.append_row(new_row.as_slice());
+        // Remember the index of the new neuron for the next iteration.
+        prev_neuron_idx = weights.shape().0 - 1;
+        // Append a zero to the bias vector.
+        layers[layer_idx].biases.append_row(vec![T::Component::zero()].as_slice());
+        // Append a column to the next layer's weight matrix with all zeros.
+        let weights = &mut layers[layer_idx + 1].weights;
+        weights.append_column(vec![T::Component::zero(); weights.shape().0].as_slice());
+    }
+    layers[end_layer_idx].weights[[end_neuron_idx, prev_neuron_idx]] = init_weight(rng, false).unwrap();
 }
 
 
