9ae7119c107e857e9a5e97528d2ec16d6d66da93
Daniil Fajnberg <mail@daniil.fajnberg.de>
2024-04-08 13:25:13 +0200

    Add `random_individual` function

--- src/evolution/init.rs
+++ src/evolution/init.rs
@@ -1,8 +1,14 @@
 use rand::Rng;
+use rand::distributions::uniform::SampleRange;
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 
+use crate::activation::{Activation, Registered};
 use crate::component::TensorComponent;
+use crate::cost_function::CostFunction;
+use crate::individual::Individual;
+use crate::layer::Layer;
+use crate::tensor::Tensor;
 
 
 pub fn init_weight<C: TensorComponent>(rng: &mut ThreadRng, can_zero: bool) -> Option<C> {
@@ -11,5 +17,37 @@ pub fn init_weight<C: TensorComponent>(rng: &mut ThreadRng, can_zero: bool) -> O
     } else {
         C::from_f32(rng.gen_range(0f32..1f32))
     }
+}
+
+
+pub fn random_individual<T: Tensor, R: Clone + SampleRange<usize>>(
+    num_layers: usize,
+    input_length: usize,
+    output_length: usize,
+    neurons_per_layer: R,
+    rng: &mut ThreadRng,
+) -> Individual<T> {
+    let mut layers = Vec::with_capacity(num_layers);
+    let mut num_neurons_previous = input_length;
+    for idx in 0..num_layers {
+        let num_neurons = if idx == num_layers - 1 { 
+            output_length
+        } else {
+            rng.gen_range(neurons_per_layer.clone())
+        };
+        let mut weights = T::zeros((num_neurons, num_neurons_previous));
+        for (_, weight) in weights.indexed_iter_mut() {
+            *weight = init_weight(rng, true).unwrap();
+        }
+        let mut biases = T::zeros((num_neurons, 1));
+        for (_, bias) in biases.indexed_iter_mut() {
+            *bias = init_weight(rng, true).unwrap();
+        }
+        layers.push(
+            Layer::new(weights, biases, Activation::get("relu").unwrap())
+        );
+        num_neurons_previous = num_neurons;
+    }
+    Individual::new(layers, CostFunction::default())
 }
 
