41eb5873a52f341c5d457d559d71975cacb2d5f7
Daniil Fajnberg <mail@daniil.fajnberg.de>
2024-04-20 11:39:43 +0200

    Add documentation; add 4-dim reference type

--- src/dimension.rs
+++ src/dimension.rs
@@ -1,26 +1,55 @@
 //! Traits for tensor dimensionality/indexing.
 
 
+/// Trait for types that express the dimensions of something.
+///
+/// Required for the [`TensorBase`]`::Dim` type.
+///
+/// [`TensorBase`]: crate::tensor::TensorBase
 pub trait Dimension {
+    /// The number of dimensions expressed by the type.
     const N: usize;
 }
 
-pub trait HasLowerDimension {
-    type Lower: HasHigherDimension + Dimension;
+/// Trait for `Dimension` types that have an associated lower dimension type.
+///
+/// Required for the [`TensorBase`]`::Dim` type.
+///
+/// [`TensorBase`]: crate::tensor::TensorBase
+pub trait HasLowerDimension: Dimension {
+    /// The associated lower dimension type.
+    ///
+    /// For consistency, that type should point back to the implementing type. (See below)
+    type Lower: Dimension + HasHigherDimension;
 }
 
-pub trait HasHigherDimension {
-    type Higher: HasLowerDimension + Dimension;
+/// Trait for `Dimension` types that have an associated higher dimension type.
+///
+/// Required for the [`TensorBase`]`::Dim` type.
+///
+/// [`TensorBase`]: crate::tensor::TensorBase
+pub trait HasHigherDimension: Dimension {
+    /// The associated higher dimension type.
+    ///
+    /// For consistency, that type should point back to the implementing type. (See below)
+    type Higher: Dimension + HasLowerDimension;
 }
 
 
+/// Alias for the 0-dimension reference type.
 pub type Dim0 = ();
 
+/// Alias for the 1-dimension reference type.
 pub type Dim1 = usize;
 
+/// Alias for the 2-dimension reference type.
 pub type Dim2 = [usize; 2];
 
+/// Alias for the 3-dimension reference type.
 pub type Dim3 = [usize; 3];
+
+/// Alias for the 4-dimension reference type.
+pub type Dim4 = [usize; 4];
 
 
 impl Dimension for Dim0 {
@@ -61,5 +90,17 @@ impl Dimension for Dim3 {
 
 impl HasLowerDimension for Dim3 {
     type Lower = Dim2;
+}
+
+impl HasHigherDimension for Dim3 {
+    type Higher = Dim4;
+}
+
+impl Dimension for Dim4 {
+    const N: usize = 4;
+}
+
+impl HasLowerDimension for Dim4 {
+    type Lower = Dim3;
 }
 
